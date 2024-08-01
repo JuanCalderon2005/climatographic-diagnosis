1fe319d0a4997767f3c405a81160d97eb630a393
Juan Calderon <juanestebancredifacil@gmail.com>
2024-08-01 16:02:08 -0500

    Implement router and page navigation

---
+++ src/pages/home/home.ts
@@ -0,0 +1,8 @@
+export function home(){
+    const root = document.querySelector('.root') as HTMLDivElement;
+
+    root.innerHTML = /*html*/ `
+        <h1>Home</h1>
+        <p>Welcome to the home
+    `;
+}
